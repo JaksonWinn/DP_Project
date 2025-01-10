d370dd22044438dea6cb5c4b5fc4b7b3f3e1d1ae
JaksonWinn <jakson.j.winn@hotmail.com>
2025-01-09 21:53:39 -0700

    Fixed an error when reading in input. In addition to adding comments to DP_Recursive_Algo.py

--- DP_Recursive_Algo.py
+++ DP_Recursive_Algo.py
@@ -1,16 +1,29 @@
+#Recursive function to find the minimum number of effoert hours for the desired number of points and the number tasks
 def grade(num_tasks, desired_points, point_val,  effort_hours, i):
+
+    #Base case for if the desired points greater than 1 and the number of tasks is 0
     if num_tasks == 0 and desired_points > 0:
         return float('inf')
+    
+    #Base case for if the desired points is 0
     if desired_points == 0 :
         return  0
+    
+    #If the desired points is greater than the number of points for the current task find the minimum of discarding
+    #the task and selecting the task and continuing
     if desired_points >= int(point_val[i]):
         return min(grade(num_tasks - 1, desired_points - int(point_val[i]), point_val, effort_hours, i + 1) + int(effort_hours[i]), 
                    grade(num_tasks - 1, desired_points, point_val, effort_hours, i + 1))
+    
+    #return the minimum of discarding the task and selecting and ending the task
     else:
         return min(int(effort_hours[i]), grade(num_tasks - 1, desired_points, point_val, effort_hours, i + 1))
 
-file_path = input()
+#Assign variables to respected values
 for i in range(0, 2):
+    file_path = input()
+
+    # Read in the input file from command line
     if i == 0:
         first_line = file_path.split()
         num_tasks = int(first_line[0])
@@ -20,9 +33,13 @@ for i in range(0, 2):
     if i == 2:
         effort_hours = input.readline().split()[::-1]
 
+#Find the result 
 result = grade(num_tasks, desired_points, point_val, effort_hours, 0)
 
+#If the result is not possible then return -1
 if result == float('inf'):
     result = -1
 
-print(result)
+#Otherwsie print the result
+else:
+    print(result)
